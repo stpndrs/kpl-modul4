f78a11050cfa3eb0db077410c9e0e85e32b7e181
Stevan Andreas <84715462+stpndrs@users.noreply.github.com>
2026-03-17 08:32:24 +0700

    feat: kode buah

---
+++ KodeBuah.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul4_103082430002
+{
+    internal class KodeBuah
+    {
+        public string getKodeBuah(string b)
+        {
+            string[] fruits =
+            {
+                "Apel",
+                "Aprikot",
+                "Alpukat",
+                "Pisang",
+                "Paprika",
+                "Blackberry",
+                "Ceri",
+                "Kelapa",
+                "Jagung",
+                "Kurma",
+                "Durian",
+                "Anggur",
+                "Melon",
+                "Semangka"
+            };
+
+            string[] codes =
+            {
+                "A00",
+                "B00",
+                "C00",
+                "D00",
+                "E00",
+                "F00",
+                "H00",
+                "I00",
+                "J00",
+                "K00",
+                "L00",
+                "M00",
+                "N00",
+                "O00"
+            };
+
+            foreach (var fruit in fruits)
+            {
+                if (fruit.Equals(b, StringComparison.OrdinalIgnoreCase))
+                {
+                    int index = Array.IndexOf(fruits, fruit);
+                    return codes[index];
+                }   
+            }
+            return "Tidak ditemukan";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using modul4_103082430002;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        KodeBuah kb = new KodeBuah();
+        Console.WriteLine(kb.getKodeBuah("Apel"));
+        Console.WriteLine(kb.getKodeBuah("Pisang"));
+        Console.WriteLine(kb.getKodeBuah("Melon"));
+        Console.WriteLine(kb.getKodeBuah("Durian"));
+        Console.WriteLine(kb.getKodeBuah("Mangga"));
+    }
+}
